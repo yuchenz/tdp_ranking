7a2832279d41e5e0ffbfad1ab9682b57bb009f41
Yuchen Zhang <yuchenz@brandeis.edu>
2018-05-10 00:02:12 -0400

    fix up baseline.sh

--- baseline.sh
+++ baseline.sh
@@ -1,14 +1,15 @@
-test_file=$1
-default_label=$2
+test_test_file=$1
+eval_test_file=$2
+default_label=$3
 
-if [ -f ${test_file}.baseline-parsed-ul ]; 
+if [ -f ${test_test_file}.baseline-parsed-ul ]; 
 then
-    mv ${test_file}.baseline-parsed-ul ~/.recycle
+    mv ${test_test_file}.baseline-parsed-ul ~/.recycle
 fi
 
 echo unlabeled ...
-python parse.py --test_file $test_file --classifier baseline --parsed_file ${test_file}.baseline-parsed-ul --default_label $default_label
-python eval.py --gold_file $test_file --parsed_file ${test_file}.baseline-parsed-ul
+python parse.py --test_file $test_test_file --classifier baseline --parsed_file ${test_test_file}.baseline-parsed-ul --default_label $default_label
+python eval.py --gold_file $eval_test_file --parsed_file ${test_test_file}.baseline-parsed-ul
 
 if [ -f ${test_file}.baseline-parsed-l ]; 
 then
@@ -16,5 +17,5 @@ then
 fi
 
 echo labeled ...
-python parse.py --test_file $test_file --classifier baseline --parsed_file ${test_file}.baseline-parsed-l --default_label $default_label --labeled
-python eval.py --gold_file $test_file --parsed_file ${test_file}.baseline-parsed-l --labeled 
+python parse.py --test_file $test_test_file --classifier baseline --parsed_file ${test_test_file}.baseline-parsed-l --default_label $default_label --labeled
+python eval.py --gold_file $eval_test_file --parsed_file ${test_test_file}.baseline-parsed-l --labeled 
